c83c1f3a875ca23f66a56cfe7dab313621396456
thzero <thzero@users.noreply.github.com>
2022-12-30 12:36:55 -0600

    updates

--- components/markup/VMarkdown.vue
+++ components/markup/VMarkdown.vue
@@ -1,27 +1,63 @@
 <template>
-	<div>
-		<!-- eslint-disable vue/no-v-html -->
-		<div
-			:class="markdownClass"
-			style="background-color: transparent"
-			v-html="display"
-		/>
-		<!--eslint-enable-->
-	</div>
+	<!-- eslint-disable vue/no-v-html -->
+	<div
+		:class="markdownClass"
+		style="background-color: transparent"
+		v-html="display"
+	/>
+	<!--eslint-enable-->
 </template>
 
 <script>
-import baseMarkdown from '@/library_vue/components/markup/baseMarkdown';
+import { useBaseMarkdownComponent } from '@/library_vue/components/markup/baseMarkdown';
+import { baseMarkdownProps } from '@/library_vue/components/markup/baseMarkdownProps';
 
 export default {
 	name: 'VtMarkdown',
-	extends: baseMarkdown,
-	setup (props) {
-		return Object.assign(baseMarkdown.setup(props), {
-		});
+	props: {
+		...baseMarkdownProps
+,	},
+	setup (props, context) {
+		const {
+			correlationId,
+			error,
+			hasFailed,
+			hasSucceeded,
+			initialize,
+			logger,
+			noBreakingSpaces,
+			notImplementedError,
+			success,
+			display,
+			markdownClass,
+			serviceMarkup
+		} = useBaseMarkdownComponent(props, context);
+
+		return {
+			correlationId,
+			error,
+			hasFailed,
+			hasSucceeded,
+			initialize,
+			logger,
+			noBreakingSpaces,
+			notImplementedError,
+			success,
+			display,
+			markdownClass,
+			serviceMarkup
+		};
 	}
 };
 </script>
 
 <style scoped>
+</style>
+
+<style>
+	@import 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css';
+	.markdown ol, ul, li {
+		margin: revert;
+		padding: revert; /* Padding is what gives the indentation */
+	}
 </style>
